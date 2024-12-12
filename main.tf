2fe0e43bce87ebd2c14c8d12161bd1a1a4283ecc
MadhumithaRJ <86790772+MadhumithaRJ@users.noreply.github.com>
2024-12-11 23:33:39 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,25 @@
+terraform {
+    required_version = "~> 1.10.2" // added latest version
+    required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.52.0"
+     }
+    }
+   }
+
 provider "aws" {
-  region = "us-east-1"
-}
+        region = "us-east-1" 
+        #profile = "default"
+	}
 
-resource "aws_instance" "example" {
-  ami           = "ami-0453ec754f44f9a4"  # Replace with a valid AMI ID
-  instance_type = "t2.micro"
-
+resource "aws_instance" "myec2" {
+	ami = "ami-0453ec754f44f9a4a" 
+	instance_type = "t2.micro"
+  key_name = "MADHU1"
   tags = {
-    Name = "ExampleInstance"
-  }
+      Name = "ExampleInstance"
+    }
 }
+
+  
